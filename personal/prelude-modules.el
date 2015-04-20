5396ef147af90a48aaa7b29912003bd52c9fb5f5
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-04-20 13:43:04 +0300

    Fix required packages

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -11,7 +11,7 @@
 (require 'prelude-evil)
 
 ;;; Programming languages support
-(require 'prelude-c)
+;; (require 'prelude-c)
 ;; (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
 ;; (require 'prelude-common-lisp)
@@ -19,19 +19,19 @@
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
 (require 'prelude-go)
-(require 'prelude-haskell)
+;; (require 'prelude-haskell)
 (require 'prelude-js)
-(require 'prelude-latex)
-(require 'prelude-lisp)
+;; (require 'prelude-latex)
+;; (require 'prelude-lisp)
 ;; (require 'prelude-ocaml)
-(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
-(require 'prelude-perl)
+;; (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
+;; (require 'prelude-perl)
 (require 'prelude-python)
-(require 'prelude-ruby)
+;; (require 'prelude-ruby)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
-(require 'prelude-xml)
+;; (require 'prelude-xml)
 ;; (require 'prelude-yaml)
